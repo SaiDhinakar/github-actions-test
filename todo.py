b21402972cf5b636b5ee74962dc57f8babefd9f7
SaiDhinakar <saidhinakars899@gmail.com>
2025-07-02 14:22:52 +0530

    todo

---
+++ todo.py
@@ -0,0 +1,17 @@
+todos = {}
+
+def add_todo(task):
+    """Add a new todo item."""
+    if task in todos:
+        print(f"Todo '{task}' already exists.")
+    else:
+        todos[task] = False
+        print(f"Todo '{task}' added.")
+
+def complete_todo(task):
+    """Mark a todo item as completed."""
+    if task in todos:
+        todos[task] = True
+        print(f"Todo '{task}' marked as completed.")
+    else:
+        print(f"Todo '{task}' does not exist.")
