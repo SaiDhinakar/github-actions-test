a91e27047dd1ba04dc05c2ad9fcb480dfa005414
SaiDhinakar <saidhinakars899@gmail.com>
2025-07-02 14:30:25 +0530

    commit

--- logs.py
+++ logs.py
@@ -1,4 +1,4 @@
-with open('./logs.txt', 'a') as f:
+with open('https://github.com/SaiDhinakar/github-actions-test/blob/main/logs.txt', 'a') as f:
     f.write('[INFO] Someone pushed their code on the repo\n')
 f.close()
 print("Log entries added successfully.")
