adb3827e6489aff5eb7757c1b43b6e0545d75f94
SaiDhinakar <saidhinakars899@gmail.com>
2025-07-02 14:28:32 +0530

    update

--- logs.py
+++ logs.py
@@ -1,4 +1,4 @@
-with open('logs.txt', 'a') as f:
+with open('./logs.txt', 'a') as f:
     f.write('[INFO] Someone pushed their code on the repo\n')
 f.close()
 print("Log entries added successfully.")
